1f994abcf185018ce00f0a0f1287ac7fb97cf3af
mawentao007 <1320963812@qq.com>
2015-07-30 23:28:21 +0800

    about

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,16 +11,19 @@ TIMEZONE = 'Asia/Hong_Kong'
 
 DEFAULT_LANG = u'en'
 
-#ARCHIVE_LIST=True
+#ARCHIVE_LIST=TrueLINKS = (('Pelican', 'http://getpelican.com/'),
 
-TEMPLATE_PAGES = {'blog.html': 'blog.html'}
-DEFAULT_PAGINATION = True
-PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
-STATIC_PATHS = ['images', 'extra/CNAME']
-DIRECT_TEMPLATES = ('categories', 'index', 'blog-index', 'blog','archives')
-POST_LIMIT = 6
 
-#ARTICLES_HOME_PAGE = 'blog-index'
+#TEMPLATE_PAGES = {'blog.html': 'blog.html'}
+#DEFAULT_PAGINATION = True
+#PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
+#STATIC_PATHS = ['images', 'extra/CNAME']
+DIRECT_TEMPLATES = ('about','categories', 'index','archives')
+#POST_LIMIT = 6
+
+ARTICLES_HOME_PAGE = 'index'
+
+SUMMARY_MAX_LENGTH = 30
 
 
 DEFAULT_DATE_FORMAT = ('%d/%b/%Y %a')
@@ -42,7 +45,7 @@ DEFAULT_DATE_FORMAT = ('%d/%b/%Y %a')
 SOCIAL = (('weibo', 'http://weibo.com/p/1005051799756001'),
          ('github','https://github.com/mawentao007'),)
 
-DEFAULT_PAGINATION = 10
+#DEFAULT_PAGINATION = 10
 
 #TEMPLATE_PAGES = {'src/books.html': 'dest/books.html',
 #                  'src/resume.html': 'dest/resume.html'}
@@ -50,7 +53,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "./themes/flat"
+#THEME = "./themes/flat"
+THEME = "./themes/notmyidea"
 
 
 PLUGIN_PATHS = ["tag_cloud", "./plugins/tag-cloud"]
@@ -58,3 +62,25 @@ PLUGINS = ["tag_cloud"]
 TAG_CLOUD_SORTING = 'random'
 TAG_CLOUD_STEPS = 4
 TAG_CLOUD_MAX_ITEMS = 100
+
+
+
+
+
+#TIMEZONE = 'Europe/Paris'
+
+#DEFAULT_LANG = u'en'
+
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+
+
+DEFAULT_PAGINATION = 3
+
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
