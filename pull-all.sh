642226e446804ef247007275d69d0790dcb0f12d
mawentao007 <1320963812@qq.com>
2015-08-01 15:09:13 +0800

    little

--- pull-all.sh
+++ pull-all.sh
@@ -1,8 +1,16 @@
+echo "**                     **"
+echo "**                     **"
 echo "****** pull Meta ******"
+echo "**                     **"
+echo "**                     **"
 cd blogMeta
 git pull
 
+echo "**                     **"
+echo "**                     **"
 echo "****** pull blog ******"
+echo "**                     **"
+echo "**                     **"
 cd ../myBlog
 cp -rf .git output
 cd output
--- submit-all.sh
+++ submit-all.sh
@@ -2,7 +2,11 @@ if [ $# != 1 ];
 then
         echo "usage: submit blogname"
 else
+        echo "**                     **"
+        echo "**                     **"
         echo "****** submit blog ******"
+        echo "**                     **"
+        echo "**                     **"
         cd myBlog
         make publish
         cp -r .git output 
@@ -11,7 +15,11 @@ else
         git commit -m "$1"
         git push
         cp -rf .git ..
+        echo "**                     **"
+        echo "**                     **"
         echo "****** submit meta ******"
+        echo "**                     **"
+        echo "**                     **"
         cd ../../blogMeta
         git add .
         git commit -m "$1"
